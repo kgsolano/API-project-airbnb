3ee13936d0bdeed617f052d4133c61c7422e955e
Kyle Solano <kylegsolano@gmail.com>
2022-09-22 13:58:01 -0700

    Add user logout backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,36 +1,18 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
-
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
 const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser);
 
-// router.get("/restore-user", (req, res) => {
-//   return res.json(req.user);
-// });
+router.use("/session", sessionRouter);
 
-// const { requireAuth } = require("../../utils/auth.js");
-// router.get("/require-auth", requireAuth, (req, res) => {
-//   return res.json(req.user);
-// });
+router.use("/users", usersRouter);
 
-
-// router.post("/test", function (req, res) {
-//   res.json({ requestBody: req.body });
-// });
-
-// const { setTokenCookie } = require("../../utils/auth.js");
-// const { User } = require("../../db/models");
-// router.get("/set-token-cookie", async (_req, res) => {
-//   const user = await User.findOne({
-//     where: {
-//       username: "Demo-lition",
-//     },
-//   });
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// });
-
+router.post("/test", (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 
 module.exports = router;
