558ab4a55d593ce7775ae4e0f815e62d8b97b9ea
Kyle Solano <kylegsolano@gmail.com>
2022-09-26 15:14:48 -0700

    Added users to seed file

--- backend/db/seeders/20220922180139-demo-user.js
+++ backend/db/seeders/20220922180139-demo-user.js
@@ -9,17 +9,37 @@ module.exports = {
         {
           email: "demo@user.io",
           username: "Demo-lition",
+          firstName: "Demo",
+          lastName: "Lition",
           hashedPassword: bcrypt.hashSync("password"),
         },
         {
           email: "user1@user.io",
           username: "FakeUser1",
+          firstName: "Fake",
+          lastName: "User",
           hashedPassword: bcrypt.hashSync("password2"),
         },
         {
           email: "user2@user.io",
           username: "FakeUser2",
+          firstName: "Faker",
+          lastName: "Person",
           hashedPassword: bcrypt.hashSync("password3"),
+        },
+        {
+          email: "user3@user.io",
+          username: "FakeUser3",
+          firstName: "Kobe",
+          lastName: "Bryant",
+          hashedPassword: bcrypt.hashSync("password4"),
+        },
+        {
+          email: "user4@user.io",
+          username: "FakeUser4",
+          firstName: "Manny",
+          lastName: "Machado",
+          hashedPassword: bcrypt.hashSync("password5"),
         },
       ],
       {}
@@ -31,7 +51,7 @@ module.exports = {
     return queryInterface.bulkDelete(
       "Users",
       {
-        username: { [Op.in]: ["Demo-lition", "FakeUser1", "FakeUser2"] },
+        username: { [Op.in]: ["Demo-lition", "FakeUser3", "FakeUser4", "FakeUser1", "FakeUser2"] },
       },
       {}
     );
