d1c0230036ad535d4766fe246208a1376ce1e8a8
Kyle Solano <kylegsolano@gmail.com>
2022-09-26 17:15:51 -0700

    created seed files for bookings and reviews

--- backend/db/seeders/20220926210347-create-bookings.js
+++ backend/db/seeders/20220926210347-create-bookings.js
@@ -1,24 +1,56 @@
-'use strict';
+"use strict";
+const { Op } = require("sequelize");
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Bookings",
+      [
+        {
+          spotId: 1,
+          userId: 1,
+          startDate: new Date('January 12, 2022'),
+          endDate: new Date('January 13, 2022'),
+        },
+        {
+          spotId: 2,
+          userId: 2,
+          startDate: new Date('February 1, 2022'),
+          endDate: new Date('February 2, 2022'),
+        },
+        {
+          spotId: 3,
+          userId: 3,
+          startDate: new Date('March 3, 2022'),
+          endDate: new Date('March 4, 2022'),
+        },
+        {
+          spotId: 4,
+          userId: 4,
+          startDate: new Date('April 4, 2022'),
+          endDate: new Date('April 5, 2022'),
+        },
+        {
+          spotId: 5,
+          userId: 5,
+          startDate: new Date('May 5, 2022'),
+          endDate: new Date('May 6, 2022'),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  down: async (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      "Bookings",
+      {
+        SpotId: {
+          [Op.in]: [1, 2, 3, 4, 5],
+        },
+      },
+      {}
+    );
+  },
 };
