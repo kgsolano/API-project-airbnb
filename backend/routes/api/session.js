2727851cb9e78158dbb3e3cebdc14f3d183fd50f
Kyle Solano <kylegsolano@gmail.com>
2022-09-29 12:20:37 -0700

    all finished

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -26,21 +26,22 @@ router.post(
   async (req, res, next) => {
     const { credential, password } = req.body;
 
-    const user = await User.login({ credential, password, firstName, lastName });
+    const user = await User.login({ credential, password});
 
     if (!user) {
-      const err = new Error('Login failed');
+      const err = new Error('Invalid Credentials');
       err.status = 401;
       err.title = 'Login failed';
       err.errors = ['The provided credentials were invalid.'];
       return next(err);
     }
 
-    await setTokenCookie(res, user);
-
-    return res.json({
-      user
-    });
+    const token = await setTokenCookie(res, user);
+    user.dataValues.token = token
+    
+    return res.json(
+      user.dataValues
+    );
   }
 );
 
