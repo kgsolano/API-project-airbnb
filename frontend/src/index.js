f5299835f93784cc8e13ca1c2739a1da4f36b507
Kyle Solano <kylegsolano@gmail.com>
2022-10-14 11:25:40 -0700

    complete modal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import './index.css';
-
-import ReactDOM from 'react-dom';
+// frontend/src/index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { restoreCSRF, csrfFetch } from './store/csrf';
-import * as sessionActions from "./store/session";
-import App from './App';
+import App from "./App";
+import { ModalProvider } from "./context/Modal";
 
 import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
@@ -23,9 +24,11 @@ if (process.env.NODE_ENV !== "production") {
 function Root() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
@@ -34,5 +37,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
