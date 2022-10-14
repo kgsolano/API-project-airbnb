861874a88980cf704f11fbddd5fd979343cfdb9e
Kyle Solano <kylegsolano@gmail.com>
2022-10-14 10:48:38 -0700

    fixed bugs with imports for signupform

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,9 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignupFormPage";
+import SignupFormPage from "./components/SignUpFormPage/";
 import * as sessionActions from "./store/session";
+
 
 function App() {
   const dispatch = useDispatch();
